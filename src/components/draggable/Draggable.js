cdf0810736a9db37c40502aef039757958e401eb
David Casanellas <davidcasanell@gmail.com>
2017-06-07 16:00:18 +0200

    feat: add captureMouseDown/Up functions and delete unnecessary handleMouseEvents function

--- src/components/draggable/Draggable.js
+++ src/components/draggable/Draggable.js
@@ -3,9 +3,19 @@ import './draggable.css';
 import handleMouseEvents from './handleMouseEvents';
 
 class Draggable extends Component {
+  captureMouseDown(e) {
+    console.log(`Mouse Down -> x: ${e.clientX}, y: ${e.clientY}`);
+  }
+  captureMouseUp(e) {
+    console.log(`Mouse Up -> x: ${e.clientX}, y: ${e.clientY}`);
+  }
   render() {
     return (
-      <div className="draggable">
+      <div
+        id="draggable"
+        className="draggable"
+        onMouseDown={this.captureMouseDown}
+        onMouseUp={this.captureMouseUp}>
       </div>
     );
   }
