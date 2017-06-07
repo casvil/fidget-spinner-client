51c6fa6727734232f389aa7407f26eee2f17bbea
David Casanellas <davidcasanell@gmail.com>
2017-06-07 15:09:46 +0200

    feat: change title tag and add draggable components

---
+++ src/components/draggable/Draggable.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './draggable.css';
+
+class Draggable extends Component {
+  render() {
+    return (
+      <div className="draggable">
+      </div>
+    );
+  }
+}
+
+export default Draggable;
