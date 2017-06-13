223eb320ff7b631f735c255fd1b3fef21189c2fe
David Casanellas <davidcasanell@gmail.com>
2017-06-13 17:29:57 +0200

    feat: add styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 style={{textAlign: 'center'}}>King of the spin!</h1>
+        <h1 style={{textAlign: 'center', fontSize: '60px'}}>King of the spin!</h1>
         <Draggable />
       </div>
     );
